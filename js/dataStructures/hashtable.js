5dee90a0e9fb053f657ce04e8a91bd189352a1ba
Elienai <elienay.soares07@gmail.com>
2022-06-22 21:59:23 -0300

    fix methods

--- js/dataStructures/hashtable.js
+++ js/dataStructures/hashtable.js
@@ -133,7 +133,7 @@ function LinkedList() {
 function Hashtable() {
   var table = [];
 
-  var valuePair = function (key, value) {
+  var ValuePair = function (key, value) {
     this.key = key;
     this.value = value;
     this.toString = function() {
@@ -143,25 +143,63 @@ function Hashtable() {
 
   this.put = function(key, value) {
     //insert element
-    var position = losolosenHashCode(key)
+    var position = loseloseHashCode(key)
     
     if(table[position] === undefined) {
       table[position] = new LinkedList()
     }
-    table[position].append(new valuePair(key, value))
+    table[position].append(new ValuePair(key, value))
   }
 
   this.remove = function(key) {
     //remove a element
-    table[losolosenHashCode(key)] = undefined
+    var position = loseloseHashCode(key)
+
+    if(table[position] === !undefined) {
+      var current = table[position].getHead()
+
+      while(current.next) {
+        if(current.element.key === key) {
+          table[position].remove(current.element)
+          if(table[position].isEmpty()) {
+            table[position] = undefined
+          }
+          return true
+        }
+        current = current.next
+      }
+      if(current.element.key === key) {
+        table[position].remove(current.element)
+        if(table[position].isEmpty) {
+          table[position] = undefined
+        }
+        return true
+      }
+    }
+    return false
   }
 
   this.get = function(key) {
-    //retorns a value if the key exists
-    return table[losolosenHashCode(key)]
+    //returns a value if the key exists
+    var position = loseloseHashCode(key)
+
+    if(table[position] !== undefined) {
+      var current = table[position].getHead()
+
+      while(current.next) {
+        if(current.element.key === key) {
+          return current.element.value
+        }
+        current = current.next
+      }
+      if(current.element.key === key) {
+        return current.element.value
+      }
+    }
+    return undefined
   }
 
-  var losolosenHashCode = function(key) {
+  var loseloseHashCode = function(key) {
     //returns the hash
     var hash = 0
     for(var i = 0; i < key.length; i++) {
@@ -198,3 +236,5 @@ hash.put('Parasect', 'bug');
 hash.put('Psyduck', 'water');
 hash.put('Poliwag', 'water');
 hash.put('Magnemite', 'eletric');
+
+hash.print()
